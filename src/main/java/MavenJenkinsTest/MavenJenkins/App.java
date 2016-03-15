5610405bf76aaf112292c0f02f989ceb6108ce4e
Whitechapel <victoremil.r.andersen@gmail.com>
2016-03-15 19:10:47 +0100

    Fourth commit

--- src/main/java/MavenJenkinsTest/MavenJenkins/App.java
+++ src/main/java/MavenJenkinsTest/MavenJenkins/App.java
@@ -5,12 +5,22 @@ package MavenJenkinsTest.MavenJenkins;
  *
  */
 public class App 
-{
-	private String test = "abetest";
-	
-	
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+{		
+	public static void main(String[] args) {
+
+		System.out.println(getHelloWorld());
+
+	}
+
+	public static String getHelloWorld() {
+
+		return "Hello World";
+
+	}
+
+	public static String getHelloWorld2() {
+
+		return "Hello World 2";
+
+	}
 }
---
+++ src/test/java/MavenJenkinsTest/MavenJenkins/TestApp1.java
@@ -0,0 +1,14 @@
+package MavenJenkinsTest.MavenJenkins;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class TestApp1 {
+
+	@Test
+	public void testPrintHelloWorld() {
+
+		Assert.assertEquals(App.getHelloWorld(), "Hello World");
+	}
+
+	}
